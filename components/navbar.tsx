6c38216b82d1d07528cf1c1f1fe78490f7dd7887
Thewhite2255 <ingedergueb@gmail.com>
2024-10-21 02:00:14 +0000

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,6 +10,7 @@ import { Menu } from 'lucide-react'
 import useCurrentUser from '@/hooks/use-current-user'
 import { useSidebar } from './ui/sidebar'
 import SearchSection from './sections/search-section'
+import { Tooltip, TooltipTrigger, TooltipContent } from './ui/tooltip'
 
 const Navbar = () => {
   const user = useCurrentUser()
@@ -38,16 +39,21 @@ const Navbar = () => {
               <SearchSection />
             </div>
             {navbarItems.map((item) => (
-              <Link
-                key={item.label}
-                href={item.href}
-                className="hidden lg:block"
-              >
-                <Button variant="ghost" size="icon" className="flex gap-2">
-                  <item.Icon className="w-5 h-5" />
-                  <span className="sr-only">{item.label}</span>
-                </Button>
-              </Link>
+              <Tooltip key={item.label}>
+                <TooltipTrigger>
+                  <Link
+                    key={item.label}
+                    href={item.href}
+                    className="hidden lg:block"
+                  >
+                    <Button variant="ghost" size="icon" className="flex gap-2">
+                      <item.Icon className="w-5 h-5" />
+                      <span className="sr-only">{item.label}</span>
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>{item.label}</TooltipContent>
+              </Tooltip>
             ))}
             <UserButton />
           </div>
